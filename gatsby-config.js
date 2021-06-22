24d1c480381f127be463ce4ec6310e44d31833f8
Felipe Saturnino <felipe@felipesaturnino.com>
2021-06-22 19:26:26 -0300

    Cleanup starter stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Felipe Saturnino`,
+    description: `Um blog sobre assuntos variados sob a luz da Psicologia.`,
+    author: `@FelipeSaturnino`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
