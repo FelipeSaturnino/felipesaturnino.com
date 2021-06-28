fb9e337c1d2a4bb3979773c9030aa5e4c82b21ea
Felipe Saturnino <felipe@felipesaturnino.com>
2021-06-28 17:58:29 -0300

    style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,15 @@ import * as React from "react"
 
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import { MainContent } from "../styles/base"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
+    <MainContent>
     <h1>404</h1>
     <p>Eita, parece que você quebrou a internet.</p>
+    </MainContent>
   </Layout>
 )
 
