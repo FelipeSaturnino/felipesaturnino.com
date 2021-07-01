5244a0fa25a4c2b1cf1d4d24c177d87eff9659b3
Felipe Saturnino <felipe@felipesaturnino.com>
2021-07-01 11:42:46 -0300

    Add Disqus comments

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -4,6 +4,7 @@ import { graphql, useStaticQuery} from "gatsby"
 import Layout from "../components/Layout"
 import Seo from "../components/seo"
 import RecommendedPosts from "../components/RecommendedPosts"
+import Comments from "../components/Comments"
 
 import * as S from "../components/Post/styled"
 
@@ -28,6 +29,7 @@ const BlogPost = ({ data, pageContext }) => {
             <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
             </S.MainContent>
             <RecommendedPosts next={next} previous={previous} />
+            <Comments url={post.fields.slug} title={post.frontmatter.title} />
         </Layout>
     )
 }
@@ -35,6 +37,9 @@ const BlogPost = ({ data, pageContext }) => {
 export const query = graphql`
     query Post($slug: String!) {
         markdownRemark(fields: {slug: {eq: $slug}}) {
+        fields {
+            slug
+        }
         frontmatter {
             title
             description
