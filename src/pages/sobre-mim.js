8660086cb9be1b2a5b71f521f74431662198007e
Felipe Saturnino <felipe@felipesaturnino.com>
2022-01-12 23:46:41 -0300

    update bio

--- src/pages/sobre-mim.js
+++ src/pages/sobre-mim.js
@@ -14,9 +14,9 @@ const AboutPage = () => (
     <MainContent>
 
         <h1>Sobre mim</h1>
-        <p>Olá! 👋 Boas vindas ao meu blog. </p>
+        <p>Olá! 👋 Boas vindas ao meu website. </p>
         <p>Meu nome é Felipe Saturnino, moro em São Paulo/SP e sou graduado em <strong>Psicologia</strong> com ênfase em Avaliação Diagnóstica pela Universidade Nove de Julho (UNINOVE), onde fui integrante do Grupo de Iniciação Científica do Curso de Psicologia na Linha de Pesquisa <em>"Processos Cognitivos, de Aprendizagem e Interação Social"</em> do Núcleo de Investigações Clínicas e Educacionais - NICE.</p>
-        <p>Decidi criar esse blog pelo meu interesse em escrever sobre assuntos variados, com foco na <em>Ciência Psicológica</em>, de forma que ela se torne mais acessível para pessoas curiosas. Espero que você possa aprender muito por aqui, assim como eu.</p>
+        <p>Decidi criar esse blog pelo meu interesse em escrever sobre assuntos variados, com foco na <em>Ciência Psicológica</em>, de forma que ela se torne mais acessível para pessoas curiosas como eu. Espero que possamos aprender muito por aqui!</p>
         <p>Se quiser conhecer mais do meu histórico acadêmico e profissional, pode acessar meu <a href="http://lattes.cnpq.br/5042886265655930" target="_blank" rel="noopener noreferrer">Currículo Lattes</a> e meu perfil no <a href="https://www.linkedin.com/in/FelipeSaturnino" target="_blank" rel="noopener noreferrer" >LinkedIn</a>. Você pode entrar em contato comigo <AniLink cover direction="left" bg={getThemeColor()} to="/contato/" duration={0.6} >clicando aqui</AniLink>.</p>
     </MainContent>
     </Layout>
